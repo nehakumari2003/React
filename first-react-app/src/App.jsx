c179aafdb45e6e3564af0be56c0bd22dd0eeea4a
nehakumari2003 <yneha4295@gmail.com>
2024-08-14 01:43:14 +0530

    file added

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+ 
+import Hello from './Hello'
+
+function App() {
+  
+  return <div>
+    <h1>
+      Im the best
+      <Hello></Hello>
+    </h1>
+  </div>
+}
+
+export default App;
---
+++ first-react-app/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello() {
+
+    let myName = 'Neha';
+    let number = 21;
+    return <h3>
+        {number} hellooooo ! my name is {myName}
+    </h3>
+}
+
+export default Hello;
