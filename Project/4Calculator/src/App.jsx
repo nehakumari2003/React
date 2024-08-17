f75abb33bac2c424620c9245dbcb6449ec5fe18b
nehakumari2003 <yneha4295@gmail.com>
2024-08-17 16:12:02 +0530

    file added

---
+++ Project/4Calculator/src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+import ButtonContainer from "./components/ButtonContainer";
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <Display></Display>
+      <ButtonContainer></ButtonContainer>
+
+    </div>
+  )
+}
+
+export default App
