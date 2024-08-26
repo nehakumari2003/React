34841978a5983c8fcfb1858cc9889faabd4816af
nehakumari2003 <yneha4295@gmail.com>
2024-08-26 15:53:52 +0530

    Calculator using React

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import ButtonContainer from "./components/ButtonContainer";
+import Display from "./components/Display";
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonContainer onButtonClick={onButtonClick}></ButtonContainer>
+    </div>
+  );
+}
+
+export default App;
